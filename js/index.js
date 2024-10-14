5d18fbb4f1aab6f1c856f2aff66991e8740210dc
jandevaishnavi <113428453+jandevaishnavi@users.noreply.github.com>
2024-10-14 17:28:02 +0530

    index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function show(){
+let a=document.getElementById("n").value;
+//document.write(a);
+/* var element=document.querySelector('.gk5');
+element.style.visibility='hidden';            
+                    
+var s = document.querySelector('.q1');
+    s.style.visibility='visible'; 
+    */
+}
